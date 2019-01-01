b8016a4c3a5657648a86127c0ddcc6401038525c
Charles Robertson <admin@cocohost.co>
2019-01-01 16:42:34 -0600

    Tweaks

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
+const path = require('path');
+
 const express = require('express');
 
 const app = express();
 
 app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, './public')));
 
 app.get('/', (request, response) => {
   response.render('index');
 });
 
 app.listen(3000, () => {
-  console.log('listening on port 3000');
+  console.log('Listening on port 3000');
 })
